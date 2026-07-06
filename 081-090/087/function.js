50072dd4631ca33644fe37c3d5c96c0452cd8f81
João Antonio <contatojoao87@gmail.com>
2026-07-06 19:54:21 -0300

    Exercicio 087

---
+++ 081-090/087/function.js
@@ -0,0 +1,5 @@
+export default function principal() {
+  return "Esta é a função principal";
+}
+
+export const VERSION = "1.0.2";
---
+++ 081-090/087/main.js
@@ -0,0 +1,5 @@
+// Crie um módulo que exporte uma função principal como padrão e uma constante chamada VERSION como exportação nomeada. Importe ambos em outro arquivo.
+
+import principal, { VERSION } from "./function.js";
+
+console.log(principal(), VERSION);
