6cf12aa699274979eb6011ed3503c8a908896de0
João Antonio <contatojoao87@gmail.com>
2026-07-06 19:49:44 -0300

    Exercicio 086

---
+++ 081-090/086/main.js
@@ -0,0 +1,5 @@
+// Crie um módulo chamado user.js que exporte como padrão um objeto contendo informações de um usuário. Importe esse objeto em main.js e exiba seus dados.
+
+import user from "./user.js";
+
+console.log(user);
---
+++ 081-090/086/user.js
@@ -0,0 +1,6 @@
+const user = {
+  nome: "João Antonio",
+  idade: 21,
+};
+
+export default user;
