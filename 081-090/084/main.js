3e8dbd615601b4ee5edcee7d30f50ff3cf6c579b
João Antonio <contatojoao87@gmail.com>
2026-07-06 19:44:03 -0300

    Exercicio 084

---
+++ 081-090/084/main.js
@@ -0,0 +1,6 @@
+// Crie um arquivo utils.js que contenha uma função responsável por transformar um texto em letras maiúsculas. Em seguida, importe essa função em main.js e exiba o resultado no console.
+import maiusculas from "./utils.js";
+
+const frase = "olá mundo!";
+
+console.log(maiusculas(frase));
---
+++ 081-090/084/utils.js
@@ -0,0 +1,3 @@
+export default function maiusculas(texto) {
+  return texto.toUpperCase();
+}
