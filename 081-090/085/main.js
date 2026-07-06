077c7f7c99381fa7999b435e44d19e9554307386
João Antonio <contatojoao87@gmail.com>
2026-07-06 19:46:36 -0300

    Exercicio 085

---
+++ 081-090/085/main.js
@@ -0,0 +1,7 @@
+// Crie um módulo chamado math.js contendo três funções: sum, subtract e multiply. Importe todas elas em outro arquivo e utilize cada uma.
+
+import { sum, subtract, multiply } from "./math.js";
+
+console.log(sum(120, 56));
+console.log(subtract(120, 56));
+console.log(multiply(10, 56));
