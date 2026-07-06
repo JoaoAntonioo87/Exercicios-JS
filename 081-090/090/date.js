2b6e35e737d5d70bb78b9399ff92065a0dcc0232
João Antonio <contatojoao87@gmail.com>
2026-07-06 20:08:10 -0300

    Exercicio 090

---
+++ 081-090/090/date.js
@@ -0,0 +1,10 @@
+const data = new Date();
+export function getCurrentYear() {
+  return data.getFullYear();
+}
+export function getCurrentMonth() {
+  return data.getMonth();
+}
+export function getCurrentDay() {
+  return data.getDate();
+}
---
+++ 081-090/090/main.js
@@ -0,0 +1,7 @@
+// Crie um módulo com diversas funções relacionadas a datas e importe todas elas utilizando um namespace.
+
+import * as Datas from "./date.js";
+
+console.log(Datas.getCurrentYear());
+console.log(Datas.getCurrentMonth());
+console.log(Datas.getCurrentDay());
